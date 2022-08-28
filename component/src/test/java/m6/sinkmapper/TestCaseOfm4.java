751acb53bcac95f909c0612f6e5efd1b2903ffd1
G Malik Basha <malik21>
2022-08-28 19:12:57 +0530

    aug is over

---
+++ component/src/test/java/m6/sinkmapper/TestCaseOfm4.java
@@ -0,0 +1,6 @@
+package m6.sinkmapper;
+
+public class TestCaseOfm4 {
+        // If you will want to know about this related testcase,
+        //refer https://github.com/wso2-extensions/siddhi-map-xml/tree/master/component/src/test
+}
---
+++ component/src/test/java/m6/sourcemapper/TestCaseOfm5.java
@@ -0,0 +1,6 @@
+package m6.sourcemapper;
+
+public class TestCaseOfm5 {
+    // If you will know about this related testcase,
+        // refer https://github.com/wso2-extensions/siddhi-map-xml/tree/master/component/src/test/
+}
